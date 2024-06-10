970bce46d1e9cf1f215cf41f4082a2748cb4783f
PrathyushaVeturi <prathyushaveturi.it@gmail.com>
2024-06-10 11:13:50 +0530

    reverse_array.cpp

--- Reverse.cpp
+++ Reverse.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 using namespace std;
 
@@ -8,36 +7,46 @@ int main() {
     int N;
     cout<<"Enter the size of the array: ";
     cin>>N;
-    
     cout<<endl;
     
     // declaration of array with the dynamic size.
     int* arr = new int[N];
-
     cout<<"Enter "<<N<<" elements to be stored in the array: "<<endl;
-    
     for(int i=0; i<N; i++){
         cin>>arr[i];
     }
     
+    // Reversing the elements => using swap()
+    int i=0; 
+    int j=N-1;
+    while(i<=j){
+        swap(arr[i],arr[j]);
+        i++;
+        j--;
+    }
+    
     //printing all the elements of the array by iterating.
     for(int i=0; i<N; i++){
-        cout<<arr[i]<<", ";
+        cout<<arr[i]<<" ";
     }
-
     return 0;
 }
 
-/*
+/* Output 1: (for even size of the array)
+Enter the size of the array: 6
 
-output:
-
-Enter the size of the array: 5
-
-Enter 5 elements to be stored in the array: 
-1 5 3 8 10
-1, 5, 3, 8, 10, 
+Enter 6 elements to be stored in the array: 
+3 7 5 9 2 8
+8 2 9 5 7 3 
 
 === Code Execution Successful ===
 
+Output 2: (for odd size)
+Enter the size of the array: 5
+
+Enter 5 elements to be stored in the array: 
+3 7 5 9 2
+2 9 5 7 3 
+
+=== Code Execution Successful ===
 */
